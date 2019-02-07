693bdd09e430651247ba19b53f5c0ee2d0c878b6
James Stronz <j.a.stronz@gmail.com>
2019-02-07 15:15:10 -0600

    Cleanup assignment-2-1 and unit test

--- assignment-2/SwapTest.cpp
+++ assignment-2/SwapTest.cpp
@@ -18,7 +18,7 @@ TEST(int_ptr_swap, int)
     int test_ptr_a = a;
     int test_ptr_b = b;
     std::stringstream s_test_ptr;
-    //
+    // execute swap by pointer
     int_ptr_swap(&test_ptr_a, &test_ptr_b);
     s_test_ptr << test_ptr_a << test_ptr_b;
     CHECK_EQUAL(s_expected_swap.str(), s_test_ptr.str());
@@ -34,7 +34,7 @@ TEST(int_ref_swap, int)
     int test_ref_a = a;
     int test_ref_b = b;
     std::stringstream s_test_ref;
-    //
+    // execute swap by reference
     int_ref_swap(test_ref_a, test_ref_b);
     s_test_ref << test_ref_a << test_ref_b;
     CHECK_EQUAL(s_expected_swap.str(), s_test_ref.str());
--- assignment-2/jstronz-2-1.cpp
+++ assignment-2/jstronz-2-1.cpp
@@ -7,13 +7,7 @@
  *   + Use int* as the argument type.
  *   + Write a second swap function using a reference (i.e., int&) as the argument type.
  */
-#include <iostream>
-#include <string>
-#include <vector>
 
-// int* a, a is an object which stores a memory address
-// int& b, alias to object provided
-//
 void int_ptr_swap(int* a, int* b) {
     int tmp_a = *a;  // a is an address. so, dereference and copy the object of type int that it points to
     *a = *b;  // overwrite object a by assigning object b (obtained by dereferencing the pointer)
@@ -25,22 +19,3 @@ void int_ref_swap(int& a, int& b) {
     a = b;
     b = tmp_a;
 }
-
-/*
-int main()
-{
-    std::vector<int> swapints(4, 0);
-    swapints[1] = 1;
-    swapints[3] = 1;
-
-    for (auto i : swapints) {
-        std::cout << i << std::endl;
-    }
-    std::cout << std::endl;
-    int_ptr_swap(&swapints[0], &swapints[1]);
-    int_ref_swap(swapints[2], swapints[3]);
-    for (auto i : swapints) {
-        std::cout << i << std::endl;
-    }
-    return 0;
-}*/
