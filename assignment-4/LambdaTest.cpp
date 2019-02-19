787d3d13a86831a7622da9c5ac26b39c4103e53f
James Stronz <j.a.stronz@gmail.com>
2019-02-19 12:00:57 -0600

    Assignment 4-1 to 4-3

---
+++ assignment-4/LambdaTest.cpp
@@ -0,0 +1,22 @@
+/*
+ *  Author: James Stronz
+ *  Course: C++PROG 711 - 2019
+ *  Assignment: 4.3
+ *  Problem Statement:
+ *   + Write a lambda function to pass the test
+ */
+#include <algorithm>
+#include "TestHarness.h"
+
+TEST(lambdaTestProblem, lambdas)
+{
+    auto values = { 2, 4, 6, 8, 10, 12 };
+    auto sum = 0;
+
+    std::for_each(values.begin(), values.end(), [&values, &sum](auto val) -> void
+                                                {
+                                                    sum += val;
+                                                });
+
+    CHECK_EQUAL(42, sum);
+}
