2c6595c155c90d5874ae8e791a83e118ca60e82b
James Stronz <j.a.stronz@gmail.com>
2019-03-21 10:17:36 -0500

    RSA checkpoint

--- project/LicenseArbiter.cpp
+++ project/LicenseArbiter.cpp
@@ -24,68 +24,78 @@
 #include <fstream>
 #include <streambuf>
 
-RSA* createPublicRSA(std::string key) {
-  RSA *rsa = NULL;
-  BIO *keybio;
-  const char* c_string = key.c_str();
-  keybio = BIO_new_mem_buf((void*)c_string, -1);
-  if (keybio == NULL) {
-      return 0;
-  }
-  rsa = PEM_read_bio_RSA_PUBKEY(keybio, &rsa, NULL, NULL);
-  return rsa;
+RSA* createPublicRSA(const char* key) {
+    RSA *rsa = nullptr;
+    BIO *keybio;
+    keybio = BIO_new_mem_buf((void*)key, -1);
+    if (keybio != nullptr)
+        rsa = PEM_read_bio_RSA_PUBKEY(keybio, &rsa, nullptr, nullptr);
+    return rsa;
 }
 
 void RSAVerifySignature(RSA* rsa, unsigned char* MsgHash, size_t MsgHashLen,
                         const char* Msg, size_t MsgLen, bool& authentic) {
-  EVP_PKEY* pubKey = EVP_PKEY_new();
-  EVP_PKEY_assign_RSA(pubKey, rsa);
-  EVP_MD_CTX* m_RSAVerifyCtx = EVP_MD_CTX_create();
+    EVP_PKEY* pubKey = EVP_PKEY_new();
+    EVP_PKEY_assign_RSA(pubKey, rsa);
+    EVP_MD_CTX* m_RSAVerifyCtx = EVP_MD_CTX_create();
 
-  if (EVP_DigestVerifyInit(m_RSAVerifyCtx, NULL, EVP_sha512(), NULL, pubKey) <= 0) {
-    ;
-  } else if (EVP_DigestVerifyUpdate(m_RSAVerifyCtx, Msg, MsgLen) <= 0) {
-    ;
-  } else if (EVP_DigestVerifyFinal(m_RSAVerifyCtx, MsgHash, MsgHashLen) == 1) {
-    authentic = true;
-    ;
-  }
+    if (EVP_DigestVerifyInit(m_RSAVerifyCtx, nullptr, EVP_sha512(), nullptr, pubKey) == 1)
+        if (EVP_DigestVerifyUpdate(m_RSAVerifyCtx, Msg, MsgLen) == 1)
+            if (EVP_DigestVerifyFinal(m_RSAVerifyCtx, MsgHash, MsgHashLen) == 1)
+                authentic = true;
     EVP_MD_CTX_free(m_RSAVerifyCtx);
 }
 
-void Base64Decode(const char* b64msg, unsigned char** buffer, size_t* length) {
-    BIO *bio;
-    BIO *b64;
-    size_t len = strlen(b64msg);
+namespace unsigned_types { 
+    typedef std::basic_string<unsigned char> string;
+}
+
+namespace crypto {
+    using unsigned_types::string;
+
+    class AES {
+        string data;
+    };
+}
+
+// Assumes no newlines or extra characters in encoded string
+std::vector<unsigned char> PreferredBase64Decode(const char* encoded)
+{
+    std::unique_ptr<BIO,BIOFreeAll> b64(BIO_new(BIO_f_base64()));
+    BIO_set_flags(b64.get(), BIO_FLAGS_BASE64_NO_NL);
+    BIO* source = BIO_new_mem_buf(encoded, -1); // read-only source
+    BIO_push(b64.get(), source);
+    const int maxlen = strlen(encoded) / 4 * 3 + 1;
+    std::vector<unsigned char> decoded(maxlen);
+    const int len = BIO_read(b64.get(), decoded.data(), maxlen);
+    decoded.resize(len);
+    return decoded;
+}
+
+void Base64Decode(std::string b64sig, unsigned char*& buffer, size_t* length) {
     size_t padding = 0;
+    for (auto br = b64sig.rbegin(); br != b64sig.rend() && padding <= 2 && *br != '='; ++br, ++padding)
+        ;
+    int decodeLen = (b64sig.length() * 3) / 4 - padding;
+    buffer = new (unsigned char*)[decodeLen + 1];
+    (buffer)[decodeLen] = '\0';
 
-    if (b64msg[len-1] == '=' && b64msg[len-2] == '=') {
-        // last two chars are =
-        padding = 2;
-    } else if (b64msg[len-1] == '=') {
-        // last char is =
-        padding = 1;
-    }
-    int decodeLen = (len * 3) / 4 - padding;
-    *buffer = (unsigned char*) malloc(decodeLen + 1);
-    (*buffer)[decodeLen] = '\0';
-
-    bio = BIO_new_mem_buf(b64msg, -1);
-    b64 = BIO_new(BIO_f_base64());
+    BIO *bio = BIO_new_mem_buf(b64sig.c_str(), -1);
+    BIO *b64 = BIO_new(BIO_f_base64());
     bio = BIO_push(b64, bio);
 
-    *length = BIO_read(bio, *buffer, strlen(b64msg));
+    *length = BIO_read(bio, buffer, b64sig.length());
     BIO_free_all(bio);
 }
 
-bool verifySignature(std::string publicKey, std::string plainText, const char* signatureBase64) {
-  RSA* publicRSA = createPublicRSA(publicKey);
-  unsigned char* encMessage;
-  size_t encMessageLength;
-  bool authentic = false;
-  Base64Decode(signatureBase64, &encMessage, &encMessageLength);
-  RSAVerifySignature(publicRSA, encMessage, encMessageLength, plainText.c_str(), plainText.length(), authentic);
-  return authentic;
+bool verifySignature(std::string publicKey, std::string plainText, std::string b64sig) {
+    RSA* publicRSA = createPublicRSA(publicKey.c_str());
+    unsigned char* encMessage;
+    size_t encMessageLength;
+    bool authentic = false;
+    Base64Decode(b64sig, encMessage, &encMessageLength);
+    RSAVerifySignature(publicRSA, encMessage, encMessageLength, plainText.c_str(), plainText.length(), authentic);
+    return authentic;
 }
 
 int main() {
@@ -105,7 +115,7 @@ int main() {
                      std::istreambuf_iterator<char>());
     //char* signature = signMessage(privateKey, plainText);
     std::cout << license << std::endl;
-    bool authentic = verifySignature(publicKey, license, signature.c_str());
+    bool authentic = verifySignature(publicKey, license, signature);
     if ( authentic ) {
         std::cout << "Authentic" << std::endl;
     } else {
